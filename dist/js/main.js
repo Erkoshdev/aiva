00359d03e62a2c9385c6f5e30de5511f48e71562
Erkosh <yerkebulan.safiolla@gmail.com>
2024-04-18 01:37:44 +0500

    prod 1

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,6 +4,7 @@ const swiper = new Swiper('.standard-slider', {
     el: '.standard-slider-pagination',
     clickable: true
   },
+  speed: 1000,
   navigation: {
     nextEl: '.standard-slider-next',
     prevEl: '.standard-slider-prev',
@@ -70,4 +71,35 @@ $(document).ready(function(){
   $("#mediaModal").on('show.bs.modal', function(){
     $("#mediaModal iframe").attr('src', url);
   });
+});
+
+//animations
+jQuery(function($) {
+
+  // Function which adds the 'animated' class to any '.animatable' in view
+  let doAnimations = function() {
+
+    // Calc current offset and get all animatables
+    let offset = $(window).scrollTop() + $(window).height(),
+      $animatables = $('.animatable');
+
+    // Unbind scroll handler if we have no animatables
+    if ($animatables.length == 0) {
+      $(window).off('scroll', doAnimations);
+    }
+
+    // Check all animatables and animate them if necessary
+    $animatables.each(function(i) {
+      let $animatable = $(this);
+      if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+        $animatable.removeClass('animatable').addClass('animated');
+      }
+    });
+
+  };
+
+  // Hook doAnimations on scroll, and trigger a scroll
+  $(window).on('scroll', doAnimations);
+  $(window).trigger('scroll');
+
 });
